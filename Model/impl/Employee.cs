2647a928a28fa11c24e24971158b9a9829b97cb0
ilker <meloonia52@gmail.com>
2021-07-28 16:42:13 +0300

    Proje dosyası ekle.

---
+++ Model/impl/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Abstract;
+
+namespace Model.impl
+{
+    class Employee : IEntity
+    {
+
+        public long id { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public long nationalIdendity { get; set; }
+
+        public string gender { get; set; } 
+
+        public string phone { get; set; }
+
+        public DateTime birtday { get; set; }
+
+
+        public string workPlace { get; set; }
+
+        public string task { get; set; }
+
+        public string reasonFordeparture { get; set; }
+
+        public DateTime starTime { get; set; }
+
+        public DateTime endTime { get; set; }
+
+        public string primarySchool { get; set; }
+        public int primarySchoolRank { get; set; }
+        public bool primarySchoolComplate { get; set; }
+
+        public string middleSchool { get; set; }
+        public int middleSchoolRank { get; set; }
+        public bool middleSchoolComplate { get; set; }
+
+        public string highSchool { get; set; }
+        public int highSchoolRank { get; set; }
+        public bool highSchoolComplate { get; set; }
+
+        public string university { get; set; }
+        public int universityRank { get; set; }
+        public bool universityComplate { get; set; }
+
+        public string driverLicance { get; set; }
+
+        public string usedPrograms { get; set; }
+
+        public string referancedInformation { get; set; }
+
+        public string referancedPhone { get; set; }
+
+
+
+
+    }
+}
---
+++ Repository/Abstract/IEntityRepostory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Model.Abstract;
+namespace Repository.Abstract
+{
+    interface IEntityRepostory<T> where T : class, IEntity, new()
+    {
+
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        public T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
